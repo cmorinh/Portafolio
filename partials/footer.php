be16ea8ea462b59422ee4761351391e7e956f2c1
cmorinh <carlos.alberto.morin@gmail.com>
2023-07-05 20:47:33 -0300

    Primera version del portafolio

---
+++ partials/footer.php
@@ -0,0 +1,6 @@
+<footer class="container-md">             
+    <div class="row mt-4 mb-2 text-center"> 
+        <p>Diseñado y construido por Carlos Morín</p>
+        </div>
+</footer>
+<script src="./js/bootstrap.bundle.min.js"></script>     
